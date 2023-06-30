ce0086d5d26ad78fb7825ac77f019502b638264a
Pascal JK <pascual.santos80@gmail.com>
2023-06-30 15:58:54 +0200

    - added fab to sliver appbar.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:custom_appbar/const/images.dart';
 import 'package:flutter/material.dart';
 
@@ -14,54 +16,67 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            expandedHeight: 250,
-            pinned: true,
-            stretch: true,
-            flexibleSpace: FlexibleSpaceBar(
-              centerTitle: true,
-              background: Image.network(
-                kBackgroundImage,
-                fit: BoxFit.cover,
+      body: Stack(
+        children: [
+          CustomScrollView(
+            slivers: [
+              _sliverAppbarWidget(),
+              SliverToBoxAdapter(
+                child: ListView.builder(
+                  itemCount: 50,
+                  primary: false,
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    return const FlutterLogo();
+                  },
+                ),
               ),
-              title: LayoutBuilder(builder: (context, constraints) {
-                top = constraints.biggest.height;
-                return AnimatedOpacity(
-                  duration: const Duration(milliseconds: 300),
-                  opacity: top <= 200 ? 1 : 0,
-                  child: const Row(
-                    children: [
-                      SizedBox(width: 12),
-                      CircleAvatar(backgroundImage: NetworkImage(kUserImage)),
-                      SizedBox(width: 12),
-                      Text('Mitsuki'),
-                    ],
-                  ),
-                );
-              }),
-            ),
+            ],
           ),
-          SliverToBoxAdapter(
-            child: ListView.builder(
-              itemCount: 50,
-              primary: false,
-              shrinkWrap: true,
-              itemBuilder: (context, index) {
-                return const FlutterLogo();
-              },
-            ),
-          )
+          _buildFab(),
         ],
       ),
     );
   }
 
+  SliverAppBar _sliverAppbarWidget() {
+    return SliverAppBar(
+      expandedHeight: 250,
+      pinned: true,
+      stretch: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        background: Image.network(
+          kBackgroundImage,
+          fit: BoxFit.cover,
+        ),
+        title: LayoutBuilder(builder: (context, constraints) {
+          top = constraints.biggest.height;
+          return AnimatedOpacity(
+            duration: const Duration(milliseconds: 300),
+            opacity: top <= 200 ? 1 : 0,
+            child: const Row(
+              children: [
+                SizedBox(width: 12),
+                CircleAvatar(backgroundImage: NetworkImage(kUserImage)),
+                SizedBox(width: 12),
+                Text('Mitsuki'),
+              ],
+            ),
+          );
+        }),
+      ),
+    );
+  }
+
   Widget _buildFab() {
-    return FloatingActionButton(
-      onPressed: () {},
-      child: const Icon(Icons.star_outline),
+    return Positioned(
+      top: Platform.isAndroid ? 250 : 270,
+      right: 20,
+      child: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.star_outline),
+      ),
     );
   }
 }
